0bfd9deee69bc2c340a2f336f01b9768c5b78926
whitelightningdev <danielmommsen@hotmail.com>
2024-09-12 08:24:23 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EduMetrics',
+  webDir: 'dist'
+};
+
+export default config;
